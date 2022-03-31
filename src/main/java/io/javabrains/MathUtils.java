6e9835b8837ad38e234a51e7c893129445c92ca4
Troy Latchman <troy.latchman@uniphore.com>
2022-03-30 23:35:35 -0400

    Creating a test class

---
+++ src/main/java/io/javabrains/MathUtils.java
@@ -0,0 +1,9 @@
+package io.javabrains;
+
+public class MathUtils {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+}
---
+++ src/test/java/io/javabrains/MathUtilsTest.java
@@ -0,0 +1,14 @@
+package io.javabrains;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathUtilsTest {
+
+    @Test
+    void test() {
+        System.out.println("This test ran");
+//        fail("Not yet implemented");
+    }
+}
