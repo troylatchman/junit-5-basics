3f7991491c55441b163b2a057446231c9efaae62
Troy Latchman <troy.latchman@uniphore.com>
2022-03-31 10:51:18 -0400

    Lifecycle hooks

--- src/test/java/io/javabrains/MathUtilsTest.java
+++ src/test/java/io/javabrains/MathUtilsTest.java
@@ -1,14 +1,33 @@
 package io.javabrains;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MathUtilsTest {
 
+    MathUtils mathUtils;
+
+    @BeforeAll
+    static void beforeAllInit() {
+        System.out.println("This needs to run before all");
+    }
+
+    @BeforeEach
+    void init() {
+        mathUtils = new MathUtils();
+    }
+
+    @AfterEach
+    void cleanup() {
+        System.out.println("Cleaning up...");
+    }
+
     @Test
     void testAdd() {
-        MathUtils mathUtils = new MathUtils();
         int expected = 2;
         int actual = mathUtils.add(1, 1);
         assertEquals(expected, actual, "The add method should add two numbers");
@@ -16,13 +35,11 @@ class MathUtilsTest {
 
     @Test
     void testDivide() {
-        MathUtils mathUtils = new MathUtils();
         assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "Divide by zero should throw");
     }
 
     @Test
     void testComputeCircleArea() {
-        MathUtils mathUtils = new MathUtils();
         assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return circle area");
     }
 }
