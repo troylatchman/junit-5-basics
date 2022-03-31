4ac85032e38317bc04ce64dccb34a04575379f2e
Troy Latchman <troy.latchman@uniphore.com>
2022-03-30 23:46:43 -0400

    Testing the add method

--- src/test/java/io/javabrains/MathUtilsTest.java
+++ src/test/java/io/javabrains/MathUtilsTest.java
@@ -8,7 +8,9 @@ class MathUtilsTest {
 
     @Test
     void test() {
-        System.out.println("This test ran");
-//        fail("Not yet implemented");
+        MathUtils mathUtils = new MathUtils();
+        int expected = 1;
+        int actual = mathUtils.add(1,1);
+        assertEquals(expected, actual, "The add method should add two numbers");
     }
 }
