447aedadbdcf81b56e6194bdf82b6891a2fd0d8a
Troy Latchman <troy.latchman@uniphore.com>
2022-03-31 10:55:09 -0400

    Test Instances They can be per_method (default) or per_class

--- src/test/java/io/javabrains/MathUtilsTest.java
+++ src/test/java/io/javabrains/MathUtilsTest.java
@@ -1,12 +1,11 @@
 package io.javabrains;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.engine.descriptor.TestInstanceLifecycleUtils;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MathUtilsTest {
 
     MathUtils mathUtils;
